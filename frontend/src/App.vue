13c8ecbdf897f0ae3ed3f579e93670df19b317a9
mariano-remixcom <mariano@remixcom.com>
2019-12-14 15:12:17 -0300

    2 col header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,16 +3,20 @@
       <div id="page-container">
         <div id="content-wrap">
           <div id="app" style="text-align:left;padding:25px">
-            <img src="../static/logo.png" width="70px" />
-            <div style="margin-bottom:25px;">Preinscripciones 2020 IFEI</div>
-            <div style="clear:both"></div>
+            <b-row>
+              <b-col style="padding-bottom: 30px"><img src="../static/logo.png" width="70px" /></b-col>
+              <b-col style="padding-top: 20px;text-align: right;">Preinscripciones 2020 IFEI</b-col>
+            </b-row>
 
-            <router-view />
 
-            <div id="footer">
-              <div class="text-center">
-                IFEI - Instituto Fueguino de Enseñanza Inglesa
-              </div>
+              <div style="clear:both"></div>
+
+              <router-view />
+
+              <div id="footer">
+                <div class="text-center">
+                  IFEI - Instituto Fueguino de Enseñanza Inglesa
+                </div>
             </div>
         </div>
         </div>
