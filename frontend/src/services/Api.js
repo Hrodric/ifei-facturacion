813db1aeb420f441764034398863e1dcfd84cd35
Hrodric <rportales7@gmail.com>
2020-01-09 10:00:22 -0300

    http://ec2-3-82-149-140.compute-1.amazonaws.com/

--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -3,7 +3,7 @@ import store from '@/store/store';
 
 export default () => {
   return axios.create({
-    baseURL: 'http://localhost:8082/',
+    baseURL: 'http://ec2-3-82-149-140.compute-1.amazonaws.com:8082/',
     headers: {
       Authorization: `Bearer ${store.state.token}`,
     },
