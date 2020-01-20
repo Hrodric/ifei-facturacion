9e8375a93566c0181204d08706e0e36a8a8148fc
Hrodric <rportales7@gmail.com>
2020-01-20 11:37:01 -0300

    endpoint frontend

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -2,10 +2,10 @@
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
-//Todo: baseURL: BACKEND_URL
+//Todo: baseURL: BACKEND_URL // http://ec2-3-82-149-140.compute-1.amazonaws.com:8082/
 
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BACKEND_URL: '"http://ec2-3-82-149-140.compute-1.amazonaws.com:8082/"'
+  // BACKEND_URL: '"http://ec2-3-82-149-140.compute-1.amazonaws.com:8082/"'
 });
--- frontend/config/prod.env.js
+++ frontend/config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BACKEND_URL: '"http://ec2-3-82-149-140.compute-1.amazonaws.com:8082"'
+  // BACKEND_URL: '"http://ec2-3-82-149-140.compute-1.amazonaws.com:8082"'
 };
