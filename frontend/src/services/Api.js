f141813a53f913a58f244415364dcba8ae9e0879
Darpa <seghezzi@gmail.com>
2020-04-01 13:03:29 -0300

    build

--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -3,7 +3,7 @@ import store from '@/store/store';
 
 export default () => {
   return axios.create({
-    baseURL: 'https://ec2-34-207-161-170.compute-1.amazonaws.com:8082',
+    baseURL: 'http://ec2-34-207-161-170.compute-1.amazonaws.com:8082',
     //baseURL: 'http://localhost:8082',
     // baseURL: BACKEND_URL,
     headers: {
